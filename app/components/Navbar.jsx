ba7d37e067cc0c50dc2b4e3a3030b47d01175836
Samuel <samuelmaglodji@gmail.com>
2025-01-13 10:40:42 +0000

    add mobile navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -13,28 +13,69 @@ const Navbar = () => {
             className="w-28 cursor-pointer mr-14"
           />
         </a>
-        <ul>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
           <li>
             <a href="#top">Home</a>
           </li>
           <li>
-            <a href="#about">About Me</a>
+            <a className="font-Ovo" href="#about">
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#services">My Services</a>
+            <a className="font-Ovo" href="#services">
+              My Services
+            </a>
           </li>
           <li>
-            <a href="#projects">My Projects</a>
+            <a className="font-Ovo" href="#projects">
+              My Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a className="font-Ovo" href="#contact">
+              Contact Me
+            </a>
           </li>
         </ul>
-        <div>
-          <a href="#contact">
+        <div className="flex items-center gap-4">
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full mt-4 font-Ovo"
+          >
             Contact <Image src={assets.arrow_icon} alt="" className="w-3" />
           </a>
+          <button className="block md:hidden ml-3">
+            <Image src={assets.menu_black} alt="" className="w-6" />
+          </button>
         </div>
+
+        {/* mobile menu goes here */}
+        <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+          <li>
+            <a href="#top">Home</a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about">
+              About Me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services">
+              My Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#projects">
+              My Projects
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact">
+              Contact Me
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
