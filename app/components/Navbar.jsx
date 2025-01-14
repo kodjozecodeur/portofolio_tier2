5457d9d6e52e953b728e78701da4f14d27f78deb
Samuel <samuelmaglodji@gmail.com>
2025-01-14 10:26:45 +0000

    add about section and fix navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,30 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 50) {
+        setIsScroll(true)
+      } else {
+        setIsScroll(false)
+      }
+    })
+  }, [])
   return (
     <div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] flex items-center justify-between ">
+      <nav className={`w-full fixed  px-5 lg:px-8 xl:px-[8%] flex items-center justify-between ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="http://">
           <Image
             src={assets.logo}
@@ -45,13 +64,23 @@ const Navbar = () => {
           >
             Contact <Image src={assets.arrow_icon} alt="" className="w-3" />
           </a>
-          <button className="block md:hidden ml-3">
+          <button className="block md:hidden ml-3" onClick={openMenu}>
             <Image src={assets.menu_black} alt="" className="w-6" />
           </button>
         </div>
 
         {/* mobile menu goes here */}
-        <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
           <li>
             <a href="#top">Home</a>
           </li>
